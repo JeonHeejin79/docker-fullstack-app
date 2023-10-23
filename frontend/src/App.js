adf6a370f0e8876780c7bcf0d55632a0dc5ba623
SKMNS\202110043 <jeonheejin@sk.com>
2023-10-23 16:52:46 +0900

    a

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -41,6 +41,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <div>도커 CI 스터디</div>
         <div className="container">
 
           {lists && lists.map((list, index) => (
