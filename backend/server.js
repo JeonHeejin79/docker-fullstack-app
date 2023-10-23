575883ec8f25d2a177cd75e47309460e819160cf
SKMNS\202110043 <jeonheejin@sk.com>
2023-10-23 15:27:12 +0900

    a

--- backend/server.js
+++ backend/server.js
@@ -12,14 +12,19 @@ const app = express();
 app.use(bodyParser.json());
 
 // 테이블 생성하기
-// db.pool.query(`CREATE TABLE lists (
-//     id INTEGER AUTO_INCREMENT,
-//     value TEXT,
-//     PRIMARY KEY (id)
-// )`, (err, results, fields) => {
-//     console.log('results', results)
-// })
- 
+db.pool.query(`CREATE TABLE lists (
+    id INTEGER AUTO_INCREMENT,
+    value TEXT,
+    PRIMARY KEY (id)
+)`, (err, results, fields) => {
+    console.log('results', results)
+})
+
+//DB lists 테이블에 있는 모든 데이터를 프론트 서베에 보내주기 
+app.get('/api/hi', function (req, res) {
+    //데이테베이스에서 모든 정보 가져오기 
+   res.status(200).send('good')
+})
 
 // API : DB lists 테이블에 있는 모든 데이터를 프론트 서버에 보내주기
 app.get('/api/values', function(req, res) {
